7cc0ddc5a70feaa433cf5a2eda68e37c34a58731
hassandaru <hassandaru@gmail.com>
2021-12-07 22:45:05 +0930

    add Hstack with SFsymbolds and adding views

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+            LinearGradient(gradient: Gradient(colors: [.blue, Color("lightBlue")]),
                            startPoint: .topLeading,
                            endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
@@ -30,10 +30,31 @@ struct ContentView: View {
                     Text("34°C")
                         .font(.system(size: 70, weight: .medium))
                         .foregroundColor(.white)
+                        .padding(.bottom)
+                    }
+                    .padding(.bottom, 40)
+                HStack(spacing: 20) {
                     
-                }
+                    weatherDayView(dayOfWeek: "TUE",
+                                   ImageName: "cloud.sun.fill",
+                                   temperature: 76)
+                    weatherDayView(dayOfWeek: "WED",
+                                   ImageName: "sun.max.fill",
+                                   temperature: 70)
+                    weatherDayView(dayOfWeek: "THU",
+                                   ImageName: "wind",
+                                   temperature: 66)
+                    weatherDayView(dayOfWeek: "FRI",
+                                   ImageName: "sunset.fill",
+                                   temperature: 60)
+                    weatherDayView(dayOfWeek: "SAT",
+                                   ImageName: "moon.stars.fill",
+                                   temperature: 55)
+                    
+                 
+             }
                 Spacer() //basically used to move the text to the top of the frame
-                
+            
             }
         }
     }
@@ -42,5 +63,34 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct weatherDay {
+    var dayOfWeek: String
+    var ImageName: String
+    var temperature: Int
+    
+}
+
+struct weatherDayView: View {
+    var dayOfWeek: String
+    var ImageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack {
+            Text(dayOfWeek)
+                .font(.system(size: 18, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            Image(systemName: ImageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            Text("\(temperature)°")
+                .font(.system(size: 28, weight: .medium))
+                .foregroundColor(.white)
+        }
     }
 }
